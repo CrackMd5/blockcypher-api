ff5435275a660474be0e0aa77c582b8f62e1ca60
CrackMd5 <45376821+CrackMd5@users.noreply.github.com>
2019-04-27 18:53:21 +0800

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Web.Helpers;
+
+namespace webhooks
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var mRead = Request.GetBufferedInputStream();
+            byte[] Buf = new byte[12800];
+            int count = mRead.Read(Buf, 0, 12800);
+            string str = Encoding.UTF8.GetString(Buf, 0, count);
+            dynamic JSON = Json.Decode(str);
+
+            string txid = JSON.hash;
+            int confirmations = JSON.confirmations;
+            
+            foreach (var o in JSON.outputs)
+            {
+                string addr = o.addresses[0];
+                 int value = o.@value;
+                 
+                
+            }
+
+
+
+
+        }
+    }
+}
